9d11b074fb0d0cc02790412ed2f3f678d4c15600
John Pate <john.pate@tlgcohort.com>
2023-11-08 14:18:54 -0600

    Lab03.2 Anonymous classes

--- Lab03.2/test/com/javatunes/personnel/EmployeeTest.java
+++ Lab03.2/test/com/javatunes/personnel/EmployeeTest.java
@@ -13,8 +13,15 @@ public class EmployeeTest {
 
     @Before
     public void setUp() {
-        emp1 = new SalariedEmployee("Mary Lou", Date.valueOf("1999-09-09"));
-        emp2 = new SalariedEmployee("Mary Lou", Date.valueOf("1999-09-09"));
+        emp1 = getEmployee();
+        emp2 = getEmployee();
+    }
+
+    private Employee getEmployee() {
+        return new Employee("Mary Lou", Date.valueOf("1999-09-09")) {
+            public double pay() {return 0; }
+            public double payTaxes() { return 0; }
+        };
     }
 
     @Test
@@ -32,5 +39,16 @@ public class EmployeeTest {
     @Test
     public void equals_shouldReturnTrue_allPropertiesSame() {
         assertEquals(emp1, emp2);
+    }
+
+    // NAME, MEMBER-LEVEL INNER CLASSES
+    private class DummyEmployee extends Employee {
+
+        public DummyEmployee(String name, Date hireDate) {
+            super(name, hireDate);
+        }
+
+        public double pay() {return 0;}
+        public double payTaxes() {return 0;}
     }
 }
